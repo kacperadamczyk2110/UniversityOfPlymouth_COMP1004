50571743ad85bcddcbd3de33c0e39cbacbb7873d
kacperadamczyk2110 <58368811+kacperadamczyk2110@users.noreply.github.com>
2024-04-07 15:53:01 +0100

    the starting point

---
+++ script.js
@@ -0,0 +1,13 @@
+var lastScrollTop = 0;
+
+window.addEventListener("scroll", function() {
+  var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
+  if (currentScroll > lastScrollTop) {
+    // Scrolling down
+    document.getElementById("navbar").style.top = "-50px"; // Hide the navigation bar
+  } else {
+    // Scrolling up
+    document.getElementById("navbar").style.top = "0"; // Show the navigation bar
+  }
+  lastScrollTop = currentScroll;
+});
